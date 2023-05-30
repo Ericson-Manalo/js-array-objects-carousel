d54f46104b6a8b4ea89d42bdc9bd8a3bef4b01f3
Ericson-Manalo <ericmana98@gmail.com>
2023-05-30 21:48:23 +0200

    added eventlistener to nextbtn

--- js/myscript.js
+++ js/myscript.js
@@ -9,8 +9,6 @@
 Milestone 0:
 > Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
 
-
-
 Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
 Milestone 2:
 > Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
@@ -22,7 +20,7 @@ BONUS 3:
 > Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. */
 
 
-const images = [
+const imagesList = [
     {
     image: 'img/01.webp',
     title: 'Marvel\'s Spiderman Miles Morale',
@@ -50,4 +48,54 @@ const images = [
     }
 ];
 
-console.log(images);
+console.log(imagesList);
+let imageSeen = 1;
+
+//richiamo il mio elemento div con classe wrapper-container
+const wrapperElement = document.querySelector('div.wrapper-container');
+
+//utilizzo la funzione richiamandone l'elemento.image contenuto dentro l'array
+imagesList.forEach((element) => {
+    wrapperElement.innerHTML += `
+<div class="wrapper-item">
+    <img src="./${element.image}" alt="">
+</div>`;
+});
+
+//richiamo div.wrapper con la posizione a cui ho assegnato imageseen e gli assegno classe active
+document.querySelectorAll('div.wrapper-item')[imageSeen].classList.add('active');
+
+
+//richiamo bottone previous button a cui darò la condizione che se cliccata andrà a diminuire di uno imageseen in modo tale da vedere la foto precedente
+const prevButton = document.querySelector('div.prev-btn');
+prevButton.addEventListener('click', function(){
+    if (imageSeen == 0){
+        imageSeen = imagesList.length - 1;
+    } else {
+        imageSeen = imageSeen - 1;
+    }
+
+    //rimuovo la classe 'attiva' in modo tale da vedere solo attiva la foto mostrata e "nascondere" quella passata
+    document.querySelector('div.wrapper-item.active').classList.remove('active');
+    document.querySelectorAll('div.wrapper-item')[imageSeen].classList.add('active');
+
+});
+
+const nextButton = document.querySelector('div.next-btn');
+nextButton.addEventListener('click', function(){
+    if(imageSeen == imagesList.length-1){
+        imageSeen = 0;
+    }else{
+        imageSeen = imageSeen + 1;
+    }
+
+    document.querySelector('div.wrapper-item.active').classList.remove('active');
+    document.querySelectorAll('div.wrapper-item')[imageSeen].classList.add('active');
+});
+
+
+
+
+
+
+
